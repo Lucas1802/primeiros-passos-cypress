2cfdb3d42c73fbb0856e82b62b3d641ad4f1fb86
Lucas Mendes <50122189+Lucas1802@users.noreply.github.com>
2026-01-07 18:38:01 -0300

    pageObject in cypress

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -11,23 +11,17 @@ const myInfoPage = new MyInfoPage()
 
 describe('Orange HRM Tests', () => {
 
-  it.only('User Info Update - Success', () => {
+  it('User Info Update - Success', () => {
     
     loginPage.accessLoginPage()
     loginPage.loginWithUser(userData.userSuccess.username, userData.userSuccess.password)
     dashboardPage.checkDashboardPage()
     menuPage.accessMyInfo()
 
-    myInfoPage.personalDetails('Lucas','Gabriel','Mendes')
-    myInfoPage.employeeDetails('GabrielGod','00000','000','2025-12-20')
-    //myInfoPage.saveForm()
-  })
-
-  it('Login - Fails', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorList.usernameField).type(userData.userFail.username)
-    cy.get(selectorList.passwordField).type(userData.userFail.password)
-    cy.get(selectorList.loginButton).click()
-    cy.get(selectorList.wrongCredentialAlert)
+    myInfoPage.fillPersonalDetails('Lucas','Gabriel','Mendes')
+    myInfoPage.fillEmployeeDetails('GabrielGod','00000','000','2025-12-20')
+    myInfoPage.fillStatus('2025-12-25')
+    myInfoPage.saveForm()
+    myInfoPage.fillCustomFields('000')
   })
 })
--- cypress/pages/myInfoPage.js
+++ cypress/pages/myInfoPage.js
@@ -5,75 +5,57 @@ class MyInfoPage {
             firstNameField: "[name='firstName']",
             middleNameField: "[name='middleName']",
             lastNameField: "[name='lastName']",
-            genericField: '.oxd-input',
-            dateCloseButton: ".--close",
+            genericField: '.oxd-input--active',
             dateField: "[placeholder='yyyy-dd-mm']",
-            submitButton: "[type='submit']",
-            //eventSucess: ".oxd-toast",
-            clickIcon: ".oxd-select-wrapper",
-            labelInput: ".oxd-radio-input"
+            genericComboBox: ".oxd-select-text--arrow",
+            secondItemComboBox: ".oxd-select-dropdown > :nth-child(2)",
+            thirdItemComboBox: ".oxd-select-dropdown > :nth-child(3)",
+            forItemComboBox: ".oxd-select-dropdown > :nth-child(6)",
+            dateCloseButton: ".--close",
+            submitButton: ".orangehrm-left-space",
+            generic: '.oxd-input',
+            genderSelector: '.oxd-radio-wrapper',
         }
 
         return selectors
     }
 
-    personalDetails(firstName, middleName, lastName) {
+    fillPersonalDetails(firstName, middleName, lastName) {
         cy.get(this.selectorList().firstNameField).clear().type(firstName)
         cy.get(this.selectorList().middleNameField).clear().type(middleName)
         cy.get(this.selectorList().lastNameField).clear().type(lastName)
     }
 
-    employeeDetails(employeeId, otherId, driverLicenseNumber, driverLicenseDate) {
-        cy.get(this.selectorList().genericField).eq(4).clear().type(employeeId)
-        cy.get(this.selectorList().genericField).eq(5).clear().type(otherId)
-        cy.get(this.selectorList().genericField).eq(6).clear().type(driverLicenseNumber)
-        cy.get(this.selectorList().genericField).eq(7).clear().type(driverLicenseDate)
+    fillEmployeeDetails(employeeId, otherId, driverLicenseNumber, driverLicenseDate) {
+        cy.get(this.selectorList().genericField).eq(3).clear().type(employeeId)
+        cy.get(this.selectorList().genericField).eq(4).clear().type(otherId)
+        cy.get(this.selectorList().genericField).eq(5).clear().type(driverLicenseNumber)
+        cy.get(this.selectorList().genericField).eq(6).clear().type(driverLicenseDate)
         cy.get(this.selectorList().dateCloseButton).click()
     }
-    statusDetails(dateBirth) {
-        // Nationality
-        cy.get(this.selectorList().clickIcon).eq(0).click().focus()
-        cy.get('body').type('{downarrow}{enter}')
-        
 
-        // Marital Status
-        cy.get(this.selectorList().clickIcon).eq(1).click().focus()
-        cy.get('body').type('{downarrow}{enter}')
-
-        // Date of Birth
-        cy.get(this.selectorList().genericField).eq(8).clear().type(dateBirth)
-        cy.get(this.selectorList().dateCloseButton).click()
-        
-        // Gender
-        cy.get(this.selectorList().labelInput).eq(1).parent().click({ force: true })
-
-        /*cy.get(this.selectorList().clickIcon).eq(0).click().then(()=>{
-            cy.contains('.oxd-select-option','Algerian').should('exist').click({force: true})
-        })
-        cy.get(this.selectorList().clickIcon).eq(1).click().then(()=>{
-            cy.contains('.oxd-select-option','Other').should('exist').click({force: true})
-        })
-        
-        
-        cy.get(this.selectorList().genericField).eq(8).clear().type(dateBirth)
-
-        cy.get(this.selectorList().dateCloseButton).click()
-        cy.get(this.selectorList().labelInput).eq(1).parent().should('exist').click({force: true})*/
-    }
-    /*customFields() {
-        cy.get(this.selectorList().clickIcon).eq(2).click()
-        cy.contains('O-').click()
-        cy.get(this.selectorList().genericField).eq(9).clear().type('123')
-    }*/
-
-    /*saveForm() {
+    saveForm() {
         cy.get(this.selectorList().submitButton).eq(0).click({force: true})
         cy.get('body').should('contain', 'Successfully Updated')
         cy.get('.oxd-toast-close')
         cy.get(this.selectorList().submitButton).eq(1).click({force: true})
         cy.get('body').should('contain', 'Successfully Updated')
         cy.get('.oxd-toast-close')
-    }*/
+    }
+
+    fillStatus(dateBirth) {
+        cy.get(this.selectorList().genericComboBox).eq(0).click({force: true})
+        cy.get(this.selectorList().secondItemComboBox).click()
+        cy.get(this.selectorList().genericComboBox).eq(1).click({force: true})
+        cy.get(this.selectorList().thirdItemComboBox).click()
+        cy.get(this.selectorList().generic).eq(8).clear().type(dateBirth) //Rever depois para usar a mesma classe que o outro GENERICFIELD
+        cy.get(this.selectorList().genderSelector).eq(1).click()
+    }
+    fillCustomFields(testeField) {
+        cy.get(this.selectorList().genericComboBox).eq(2).click({force: true})
+        cy.get(this.selectorList().forItemComboBox).click()
+        cy.get(this.selectorList().genericField).eq(9).clear().type(testeField)
+    }    
 }
 
 export default MyInfoPage
